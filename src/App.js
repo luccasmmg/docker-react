aba4d5777f12bc13df2e171210e6cb40c1bb6f8d
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2020-03-27 00:04:49 -0300

    Change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to not bye wow crazy shiet reload.
+          Edit <code>src/App.js</code> Changed into feature branch. 
         </p>
         <a
           className='App-link'
